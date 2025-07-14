a3a7576dcc7f6fbf2bffa9f1f733b2c2047c31f0
YYBer <liyuanying225@gmail.com>
2025-07-14 12:01:43 +0200

    remove sql files

--- add_parent_comment_id.sql
+++
@@ -1,7 +0,0 @@
--- Add parent_comment_id column to bounty_comments table to support threaded replies
-
-ALTER TABLE public.bounty_comments 
-ADD COLUMN parent_comment_id uuid REFERENCES public.bounty_comments(id) ON DELETE CASCADE;
-
--- Create index for better performance when querying replies
-CREATE INDEX idx_bounty_comments_parent_id ON public.bounty_comments(parent_comment_id);
